352350fedaf9b908c0e57d8d5b9dd891bcbd168b
Thomas Taylor <thomas.taylor@kineo.com>
2020-03-12 14:55:16 +0000

    Rename module

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const responsibilitiesModule = require('./lib/responsibilitiesModule');
-module.exports = responsibilitiesModule;
+const rolesModule = require('./lib/rolesModule');
+module.exports = rolesModule;
